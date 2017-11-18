e92b9bb9081339eab2c522192e20bc3d282209fa
Nakajima Kazuki <JP22274@w022571708172m.local>
2017-11-18 14:06:52 +0900

    dev

---
+++ service/resas.js
@@ -0,0 +1,36 @@
+"use strict"
+const request = require("request");
+Promise = require("bluebird");
+Promise.promisifyAll(request);
+
+const endpoint = "https://opendata.resas-portal.go.jp";
+
+module.exports = class ServiceResas {
+    constructor(api_key){
+        this.api_key = api_key;
+    }
+
+    get_prefectures(){
+        return this._get("api/v1/prefectures");
+    }
+
+    _get(path){
+        let url = endpoint + "/" + path;
+        let headers = {
+            "X-API-KEY": this.api_key
+        }
+        return request.getAsync({
+            url: url,
+            headers: headers,
+            json: true
+        }).then((response) => {
+            if (response.statusCode != 200){
+                return Promise.reject(new Error(`GET ${url} failed.`));
+            } else if (response.body.statusCode != 200){
+                return Promise.reject(new Error(response.body.description));
+            } else {
+                return response.body;
+            }
+        });
+    }
+}
---
+++ skill/show-prefectures.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const resas = new require("../service/resas")(process.env.RESAS_API_KEY);
+
+module.exports = class SkillShowPrefecture {
+    constructor(){
+        this.clear_context_on_finish = true;
+    }
+
+    finish(bot, event, context, resolve, reject){
+        return resas.get_prefectures().then((response) => {
+            let text = "";
+            for (let pref of response.result){
+                text += pref.prefName + ",";
+            }
+            return bot.reply({
+                type: "text",
+                text: text + "です。"
+            });
+        }).then((response) => {
+            return resolve();
+        });
+    }
+}
