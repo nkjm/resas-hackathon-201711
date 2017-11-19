3770b7aed1831444703736269ff2640e0a822824
Nakajima Kazuki <JP22274@w022571708172m.local>
2017-11-19 13:44:57 +0900

    dev

---
+++ skill/show-estate-drawing.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const Resas = require("../service/resas");
+const resas = new Resas(process.env.RESAS_API_KEY);
+const debug = require("debug")("bot-express:skill");
+const db = require("../service/db");
+
+module.exports = class SkillShowEstateDrawing {
+    constructor(){
+        this.required_parameter = {
+            estate: {
+                message_to_confirm: { // dummy.
+                    type: "text",
+                    text: "どの物件ですか？"
+                }
+            }
+        }
+    }
+
+    finish(bot, event, context, resolve, reject){
+        let messages = [{
+            type: "text",
+            text: "こちらになります。"
+        },{
+            type: "image",
+            orginalContentUrl: context.confirmed.estate.drawing_orig_url,
+            previewImageUrl: context.confirmed.estate.drawing_prev_url
+        }];
+        
+        return bot.reply(messages).then((response) => {
+            return resolve();
+        });
+    }
+}
